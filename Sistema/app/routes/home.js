54f14bc2b7b4effe7043ab2a938c6b785b6f7475
Luis Henrique Belisario <luishenriquebelisario@iMac-de-Luis.local>
2020-04-06 22:00:34 -0300

    Criacao modulo de integração ao Banco de Dados

--- Sistema/app/routes/home.js
+++ Sistema/app/routes/home.js
@@ -1,22 +1,14 @@
+var dbConnection = require('../../config/dbConnection')
+
+
 module.exports = function(app) {
 
+    var connection = dbConnection()
 
     app.get('/', function(req, res) {
-        var mysql = require('mysql')
-
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: 'rootpassword',
-            database: 'projeto_saude'
-        })
 
         connection.query('select * from noticias', function(erro, result) {
-            res.send(result)
-        })
-
-        app.get('/', function(req, res) {
-            res.render('home/index')
+            res.render('home/index', {noticias : result})
         })
     })
 }
