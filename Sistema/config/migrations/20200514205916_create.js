082a4f43d01e77e4e594b647af2cd9e3819cbb71
Tao Hansen <tao.santos@somosicev.com>
2020-05-14 21:05:55 -0300

    create db

--- Sistema/config/migrations/20200514205916_create.js
+++ Sistema/config/migrations/20200514205916_create.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('noticias', function(table){
+        table.int('id').primary();
+        table.string('titulo').notNullable();
+        table.string('noticia').notNullable();
+        table.string('autor').notNullable();
+        table.string('data').notNullable();
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable(noticias);
 };
